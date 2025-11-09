9d386ee7c9cf62d45ee524815b16240cad8719de
Lucas <lucasandrade595@gmail.com>
2025-11-09 11:12:13 -0300

    feat: first api spring boot

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -2,11 +2,20 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //Para ser springBoot precisa da anotacao
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello";
+    }
 }
+
